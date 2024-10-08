c7f2f693143b0c8b0920cb0092e90df6deb572d8
Salinepng <salinengatha@gmail.com>
2024-10-08 16:30:21 +0300

    Add files via upload

---
+++ electric bill.c
@@ -0,0 +1,60 @@
+//electricity bill calcultion
+#include<stdio.h>
+int main(){
+	int customerId,unitconsumed;
+	char customername[50];
+	float chargeperunit,surcharge=0,totalbill;
+	
+	printf("Enter the Customers name:");
+	scanf("%s",&customername);
+	
+	printf("Enter customers id no:");
+	scanf("%d",&customerId);
+	
+	printf("Enter the unitconsumed:");
+	scanf("%d",&unitconsumed);
+
+	
+	//determining charge per unit
+	
+	if (unitconsumed <=199){
+	   chargeperunit = 1.20;}
+	
+	else if(unitconsumed >=200 && unitconsumed <=400) {
+        chargeperunit = 1.50;}	
+        
+	else if(unitconsumed>=400 && unitconsumed <=600) {
+	   chargeperunit = 1.80;}
+	   
+	else {
+	 chargeperunit = 2.00;}
+
+	//calculate the total bill
+totalbill = unitconsumed*chargeperunit;
+
+	//if total bill exceeds 400 shillings,15% surcharge is charged
+	if (totalbill >400){
+		surcharge=totalbill*0.15 ;
+		totalbill+=surcharge;
+	 
+		//the minimum should be 100
+	if (totalbill <100){   
+	     totalbill=100;
+		 
+		 	
+		 }
+		 	
+		 }
+		 
+		 	
+		 
+		printf("Customers Id:%d\n",customerId);
+		printf("Customers name:%s\n",customername);
+		printf("Units consumed:%d\n",unitconsumed);
+		printf("Charge per unit:%.2f\n",chargeperunit);
+		printf("Total bill:%.2f\n",totalbill);
+		
+	
+
+	 return 0;
+	 }
